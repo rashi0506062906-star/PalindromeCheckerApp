f05e76274949a2144fe31fbc1481a394597a17a9
rashi0506062906-star <rashi050606.2906@gmail.com>
2026-02-26 15:44:34 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,22 +1,24 @@
-public class PalindromeCheckerApp {
+public class PalindromeProgram {
 
     public static void main(String[] args) {
 
         // Hardcoded string
         String str = "madam";
 
-        // Reverse the string
+        // Variable to store reversed string
         String reversed = "";
 
+        // Reverse the string
         for (int i = str.length() - 1; i >= 0; i--) {
-            reversed += str.charAt(i);
+            reversed = reversed + str.charAt(i);
         }
 
-        // Check if palindrome
+        // Check palindrome using if-else
         if (str.equals(reversed)) {
             System.out.println("The string \"" + str + "\" is a palindrome.");
         } else {
             System.out.println("The string \"" + str + "\" is not a palindrome.");
         }
+
     }
 }
