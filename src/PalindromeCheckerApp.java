5f17cec7d165b6073587252e494cf50f05993c85
rashi0506062906-star <rashi050606.2906@gmail.com>
2026-02-26 15:42:01 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,22 @@
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String str = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        // Check if palindrome
+        if (str.equals(reversed)) {
+            System.out.println("The string \"" + str + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + str + "\" is not a palindrome.");
+        }
+    }
 }
